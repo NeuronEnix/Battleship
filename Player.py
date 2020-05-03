535b7ee2607785b2c842a4ccb5a1e698eebb2e20
neuron909 <kaushikrb909@gmail.com>
2020-05-03 13:55:57 +0530

    added default char to button

--- Player.py
+++ Player.py
@@ -5,15 +5,14 @@ class Player:
     def __init__( self, baseData = baseData ) :
         self.base = Base( baseData[0],baseData[1],baseData[2] )
         self.bgAudio = mdi.aud( 'audio/bg/0')
-        # self.bgAudio.play()
         
     def mouseMotion( self, xy ):
         self.base.mouseMotion( xy )
-    def mouseDrag( self, xy, button ):
-        self.base.mouseDrag( xy, button )
-    def mousePress( self, xy, button ):
-        self.base.mousePress( xy, button )
-    def mouseRelease( self, xy, button ):
+    def mouseDrag( self, xy ):
+        self.base.mouseDrag( xy )
+    def mousePress( self, xy, button = 'l' ):
+        self.base.mousePress( xy, button = 'l' )
+    def mouseRelease( self, xy, button = 'l' ):
         self.base.mouseRelease( xy, button )
     def draw( self ) :
         self.base.draw()
