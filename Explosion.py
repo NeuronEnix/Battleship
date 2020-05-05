ac3358a0b580871a1038d53adb4097f0cf297452
neuron909 <kaushikrb909@gmail.com>
2020-05-05 09:22:43 +0530

    GameModel <- Explosion

--- Explosion.py
+++ Explosion.py
@@ -1,54 +1,56 @@
 import model as mdl
-from GameModel import GameModel
-from Global import explosionPath, smokePath, explosionAudioPath
 import media as mdi
-import time
+from tGameModel import GameModel
+import Global as glb
 
-def explosionDuration() :
-    return time.time() + 1
+expPath = 'img/explosion/0'
+smokePath = 'img/smoke/0'
+expAudPath = 'audio/explosion/0'
+massExpAudPath = 'audio/explosion/1'
 
 class Explosion( GameModel ) :
-    def __init__( self, xy, wh, rc ) :
-        super().__init__( xy, wh, rc )
+    def __init__( self, xy, wh, rc, batch ) :
+        super().__init__( xy, wh, rc, batch, glb.gExpGrid )
+        self.batch = batch
         self.explodedInd = set()
-        self.explosionList = []
-        self.smokeList = []
-        self.explosionAudio = mdi.aud( explosionAudioPath + '0' )
-        self.massExplosionAudio = mdi.aud( explosionAudioPath + '1' )
 
     def explodeAt( self, xy ) :
         ind = str( self.XYToIndex( xy ) )
         if ind in self.explodedInd:
             return False
-        self.explosionAudio.play()
+
+        xy = self.XYToXY( xy )
+        mdl.gif( xy, expPath, self.subWH, self.batch, glb.gExp , oneTime = True )
+        mdi.aud( expAudPath ).play()
+
         self.explodedInd.add( ind )
         
-        xy = self.XYToXY( xy )
-        
-        self.explosionList.append( self.newExplosion( xy ) )
-        
-        self.smokeList.append( mdl.gif( xy, smokePath+'0', self.subWH ))
-        
+        mdl.gif( xy, smokePath, self.subWH, self.batch, glb.gSmoke)
         return True
     
-    def newExplosion( self, xy ) :
-        return [
-            explosionDuration(),
-            mdl.gif( xy, explosionPath+'0',  self.subWH )
-        ]
         
     def initiateMassExplosion( self ) :
         for i in range( self.rc[0] ) :
             for j in range( self.rc[1] ) :
                 xy = self.indexToXY( [i,j] )
-                self.explosionList.append(  self.newExplosion( xy ) )
-        self.massExplosionAudio.play()
+                mdl.gif( xy, expPath, self.subWH, self.batch, glb.gExp, oneTime = True )
+        mdi.aud( massExpAudPath ).play()
     
-    def draw( self ) :
-        for smoke in self.smokeList :
-            smoke.draw()
-        if self.explosionList :
-            curTime = time.time()
-            self.explosionList = [ explosion for explosion in self.explosionList if explosion[0] > curTime ]
-            for explosion in self.explosionList : 
-                explosion[1].draw()
+    # def draw( self ) :
+    #     for smoke in self.smokeList :
+    #         smoke.draw()
+    #     if self.explosionList :
+    #         curTime = time.time()
+    #         self.explosionList = [ explosion for explosion in self.explosionList if explosion[0] > curTime ]
+    #         for explosion in self.explosionList : 
+    #             explosion[1].draw()
+
+    # def s_xy( self, xy ) :
+    #     super().s_xy( xy )
+    #     self.model.x, self.model.y = xy[0], xy[1]
+    # xy = property( GameModel.g_xy, s_xy )
+
+    # def s_wh( self, wh ) :
+    #     super().s_wh( wh )
+    #     # self.model.width, self.model.height = wh[0], wh[1]
+    # wh = property( GameModel.g_wh, s_wh)
