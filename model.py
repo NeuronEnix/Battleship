6aa28bf73d60bcb4a9928496f37b05aa74694231
neuron909 <kaushikrb909@gmail.com>
2020-05-05 09:24:30 +0530

    added oneTimeSprite and CustomGroup <- manipulation OpenGL

--- model.py
+++ model.py
@@ -1,6 +1,28 @@
 import pyglet.sprite as pySpt
 import pyglet.resource as pyRes
 import pyglet.graphics as pyGra
+from pyglet.gl import GL_LINES, GL_QUADS, GL_BLEND, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA
+from pyglet.gl import  glEnable, glBlendFunc, glDisable
+def genGroup( group ) :
+    if group != None : group = pyGra.OrderedGroup( group )
+    return group
+
+class OneTimeSprite( pySpt.Sprite ) :
+    def __init__( self, xy, obj,batch = None, group = None ) :
+        super().__init__( obj, x=xy[0], y=xy[1], batch = batch, group = group)
+
+    def on_animation_end( self ) :
+        self.delete()
+        
+class CustomGroup( pyGra.Group ) :
+    def set_state( self ) :
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        
+    def unset_state( self ) :
+        glDisable(GL_BLEND)
+        pass
+    
 def scale( model, newWH ):
     if newWH[0] :   model.scale_x = newWH[0] / model.width
     if newWH[1] :   model.scale_y = newWH[1] / model.height
@@ -12,21 +34,21 @@ def objAnchorXY( obj ) :
     return obj
 
 
-def gif( xy, path, wh = [None, None]):
+def gif( xy, path, wh = [None, None], batch = None, group = None, oneTime = False):
     obj = pyRes.animation( path + '.gif' )
-    model = pySpt.Sprite( obj, x=xy[0], y=xy[1] )
+    if oneTime : model = OneTimeSprite( xy, obj, batch = batch, group = genGroup( group ) )
+    else       : model = pySpt.Sprite( obj, x=xy[0], y=xy[1], batch = batch, group = genGroup( group ) )
     model = scale( model, wh )
     return model
 
-def img( xy, path, wh = [None, None], anchorXY = False ):
+def img( xy, path, wh = [None, None], batch = None,group = None, anchorXY = False ):
     obj = pyRes.image( path + '.png')
-    if anchorXY:
-        obj = objAnchorXY( obj )
-    model = pySpt.Sprite( obj, x=xy[0], y=xy[1] )
+    if anchorXY: obj = objAnchorXY( obj )
+    model = pySpt.Sprite( obj, x=xy[0], y=xy[1], batch = batch, group = genGroup( group ) )
     model = scale( model, wh)
     return model
 
-def grid( xy, wh, rc ):
+def grid( xy, wh, rc, batch = None, group = None ):
     verts = [
         xy[0], xy[1],               xy[0]+wh[0], xy[1],
         xy[0]+wh[0] ,xy[1],         xy[0]+wh[0], xy[1]+wh[1],
@@ -39,6 +61,19 @@ def grid( xy, wh, rc ):
     for i in range( 1, rc[0] ) :
         y = round( xy[1] + wh[1] / rc[0] * i )
         verts.extend( [ xy[0], y,   xy[0] + wh[0], y ] )
-    
-    grid = pyGra.vertex_list( len( verts ) // 2, ( 'v2i', verts ) )
+
+    if batch :
+        grid = batch.add( len( verts ) // 2, GL_LINES, genGroup( group ) ,( 'v2i', verts ) )
+    else     :
+        grid = pyGra.vertex_list( len( verts ) // 2, ( 'v2i', verts ) )
     return grid
+
+def quad( xy, wh, color = [0, 255, 242, 50 ], batch = None, group = None, blend = True  ):
+    x,y = xy[0], xy[1]
+    w, h = wh[0], wh[1]
+    vertex = ( "v2i", (    x,  y,  x+w,    y,  x+w,y+h,    x,y+h   )    )
+    color = (   'c4B', ( color*4 ) )
+    if blend    :   group = CustomGroup( pyGra.OrderedGroup( group ) )
+    else        :   group = genGroup( group )
+    return batch.add( 4, GL_QUADS, group , vertex, color )
+    
