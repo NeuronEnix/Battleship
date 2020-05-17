d48b145c73134e2b2f0b720931e135afc2eeda2b
neuron909 <kaushikrb909@gmail.com>
2020-05-17 22:33:33 +0530

    Combined all menu to mainMenu

--- Menu.py
+++ Menu.py
@@ -6,135 +6,90 @@ import MultiPlayer as mp
 import SidePanel as sp
 import CustomSocket
 import pyglet.graphics as pyGra
+from pyglet.window import key
+CONNECTING = 2
+JOINING = 1
 class MainMenu( sp.SidePanel ):
     def __init__( self ) :
-        GS = glb.GameStatus
-        batch = pyGra.Batch()
-        headerText = 'Battleship'
-        optionList = [
-            [ 'Single Player', GS.SINGLE_PLAYER ],
-            [ 'Multi Player' , GS.MULTI_PLAYER  ],     #Below empty list indicate that nothing should be displayed and selected
-            [ 'LAN'          , GS.LAN           ],     [], [], [],
-            [ 'Exit'         , GS.EXIT          ]
-        ]
-        bgPath = glb.Path.bgGif
-        group = glb.Group.MAIN_MENU
+        self.lanStatus = None   ;   self.port = ''
+        self.initInfo()         ;   bgPath = glb.Path.bgGif
+        batch = pyGra.Batch()   ;   group = 0
+
         super().__init__(
-            headerText, optionList,
+            self.mainMenuInfo[0], self.mainMenuInfo[1],
             batch = batch, group = group , bgPath = bgPath  
         )
         glb.onScreen = self
-    def mousePress( self, xy, button ) :
-        if button == 'l' :
-            GS = glb.GameStatus
-            status = super().mousePress( xy, button )
-            if status == GS.MULTI_PLAYER :
-                mp.MultiPlayer()
-            elif status == GS.LAN :
-                LanMenu()
-            elif status == GS.EXIT :
-                sys.exit()
+        
+    def mainMenu( self )    :  self.resetPanel( self.mainMenuInfo ) 
+    def lanMenu( self )     :   self.resetPanel( self.lanMenuInfo) ; self.lanStatus = None
+
+    def hostMenu( self )    :
+        socket = CustomSocket.CustomSocket()
+        self.hostMenuInfo[1][1][0] = str( socket.port )
+        self.resetPanel( self.hostMenuInfo )
+        self.connect( socket )
     
-class LanMenu( sp.SidePanel ) :
-    def __init__( self ) :
-        GS = glb.GameStatus
-        self.port = ''
-        self.gs = GS.LAN
-        self.onCancel = glb.onScreen
-        batch = pyGra.Batch()
-        headerText = 'L  .  A  .  N'
-        LanOptionList = [
-            [ 'Host', GS.HOST ],
-            [ 'Join' , GS.JOIN  ],     #Below empty list indicate that nothing should be displayed and selected
-            [], [], [], [],
-            [ 'Cancel'         , GS.CANCEL          ]
-        ]
-        bgPath = glb.Path.bgGif
-        group = glb.Group.MAIN_MENU
-        super().__init__(
-            headerText, LanOptionList,
-            batch = batch, group = group , bgPath = bgPath  
-        )
-        glb.onScreen = self
-    def mousePress( self, xy, button ) :
-        if button == 'l' :
-            GS = glb.GameStatus
-            status = super().mousePress( xy, button )
-            if status == GS.HOST :
-                self.host()
-            elif status == GS.JOIN :
-                self.join()
-            elif status == GS.CONNECT :
-                self.gs = GS.gameStatus = GS.CONNECT
-                self.socket = CustomSocket.CustomSocket()
-                if self.port :
-                    self.port = int(self.port)
-                self.setOptionAt( 5, ['Connecting...'])
-            elif status == GS.CANCEL :
-                glb.onScreen = self.onCancel
-
-    def keyPress( self, num ) :
-        if self.gs == glb.GameStatus.JOIN :
-            if num == -1 :
-                self.port = self.port[:-1]
-            elif len(self.port) < 5:
-                self.port += str(num)
-            self.optionLabel[1].text = 'ID : ' + self.port
-
-    def host( self ) :
-        self.gs = glb.GameStatus.CONNECT
-        self.socket = CustomSocket.CustomSocket()
-        self.setOptionLabels([
-            ['Your ID :'],
-            [ str(self.socket.port) ],[],[],[],
-            ['Waiting...'],
-            ['Cancel',glb.GameStatus.CANCEL]
-        ])
-        
-    def join( self ) :
-        self.port = ''
-        self.gs = glb.GameStatus.gameStatus = glb.GameStatus.JOIN
-        self.setOptionLabels([
-            ['Enter'],['ID : '],[],[],[],
-            ['Connect',glb.GameStatus.CONNECT],
-            ['Cancel', glb.GameStatus.CANCEL]
-        ])
-        
-    def update( self ) :
-        if self.gs == glb.GameStatus.CONNECT :
-            if self.socket.isConnected( self.port ) :
-                LAN.Lan( self.socket )
+    def joinMenu( self ) :
+        self.resetPanel( self.joinMenuInfo )
+        self.lanStatus = JOINING
+    
+    def connect( self, socket = None ) :
+        if socket == None : socket = CustomSocket.CustomSocket()
+        if self.lanStatus == JOINING : self.port = int( self.port ) ; self.optionLabels[-2].text = 'Connecting...' ; self.optionList[ -2 ] = ['Connecting...']
+        self.socket = socket    ;   self.lanStatus = CONNECTING
                 
-            
-class PauseMenu( sp.SidePanel ) :
-    def __init__( self ) :
-        self.onResume = glb.onScreen
-        batch = pyGra.Batch()
-        headerText = 'Paused   '
-        gs = glb.GameStatus
-        optionList = [
-            [ 'Resume',     gs.RESUME       ],
-            [ 'Restart' ,   gs.MAIN_MENU    ],     #Below empty list indicate that nothing should be displayed and selected
-            [ 'Main Menu',  gs.MAIN_MENU    ],     [], [], [],
-            [ 'Exit',       gs.EXIT         ]
-        ]
-        group = glb.Group.MAIN_MENU
-        super().__init__(
-            headerText, optionList,
-            batch = batch, group = group ,fullScreenBlend=True 
-        )
-        self.batch = batch
-        glb.onScreen = self
-    def mousePress( self, xy, button ) :
-        if button == 'l' :
-            status = super().mousePress( xy, button )
-            if status == glb.GameStatus.MAIN_MENU :
-                MainMenu()                
-            elif status == glb.GameStatus.EXIT :
-                sys.exit()
-            elif status == glb.GameStatus.RESUME :
-                glb.onScreen = self.onResume
-    def draw( self ) :
-        self.onResume.draw()
-        self.batch.draw()
+    def update( self ) :
+        if self.lanStatus == CONNECTING and self.socket.isConnected( self.port ) : LAN.Lan( self.socket )
+
+    def keyPress( self, btn ) :
+        if self.lanStatus == JOINING and self.isValid( btn ) :
+            num = self.toNum( btn )
+            if num == -1 : self.port = self.port[:-1] # On Backspace
+            elif num == -2 : self.connect() ; return # On Enter
+            elif len(self.port) < 5 : self.port += str(num)
+            self.optionLabels[1].text = 'ID : ' + self.port
         
+    def initInfo( self ) :
+        self.mainMenuInfo = [ 'Battleship',
+            [
+                [ 'Single Player', self.doNothing   ],
+                [ 'Multi Player' , mp.MultiPlayer   ],     
+                [ 'LAN'          , self.lanMenu     ],     [], [], [],
+                [ 'Exit'         , sys.exit         ]
+            ]
+        ]
+        self.lanMenuInfo = [ 'L  A  N',
+            [
+                [ 'Host', self.hostMenu ],
+                [ 'Join' , self.joinMenu ], [],[],[],[],
+                [ 'Cancel', self.mainMenu ]
+            ]
+        ]    
+        self.hostMenuInfo = [ 'Hosting',
+            [
+                ['Your ID :'],
+                ["""Port goes here"""], [],[],[],
+                ['Waiting...'],
+                ['Cancel',self.lanMenu]
+            ]
+        ]
+        self.joinMenuInfo = [ 'Join',
+            [
+                ['Enter'],['ID : '],[],[],[],
+                ['Connect',self.connect],
+                ['Cancel', self.lanMenu ]
+            ]
+        ]
+
+    @staticmethod
+    def isValid( b ) :
+        if key.NUM_0 <= b <= key.NUM_9 or key._0 <= b <= key._9 or b == key.BACKSPACE or b == key.ENTER: return True
+        return False
+    @staticmethod
+    def toNum( b ) :
+        if b == key.BACKSPACE : return -1
+        if b == key.ENTER : return -2
+        if key.NUM_0 <= b <= key.NUM_9 :
+            return b - key.NUM_0
+        return b - key._0    
