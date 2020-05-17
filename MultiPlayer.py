f50c73cdecac1a2696f478b2a53d88193496cdff
neuron909 <kaushikrb909@gmail.com>
2020-05-17 22:33:59 +0530

    refactored: used callBacks

--- MultiPlayer.py
+++ MultiPlayer.py
@@ -1,32 +1,25 @@
 import Global as glb
-import GameMaster 
+import GameMaster as GM
 
-GS = GameMaster.GS
-GameMaster = GameMaster.GameMaster
 
 cc = 0
 SET_PLAYER_1 = cc ; cc += 1
 SET_PLAYER_2 = cc ; cc += 1
 BATTLEFIELD = cc ; cc += 1
-class MultiPlayer ( GameMaster ):
+class MultiPlayer ( GM.GameMaster ):
     def __init__( self ) :
         super().__init__()
         self.status = SET_PLAYER_1
-        self.setPlayer1( 'Player  1    ' )
+        self.setPlayer1( 'Player 1', onConfirm = self.playerSetupSeq )
         glb.onScreen = self
 
     def playerSetupSeq( self ) :
         if self.status == SET_PLAYER_1 :
             self.status = SET_PLAYER_2
-            self.setPlayer2( 'Player  2    ' )
+            self.setPlayer2( 'Player  2', onConfirm = self.playerSetupSeq )
         else:
-            self.setBattleField( [self.archivePlayer1(), self.archivePlayer2()], 1 ,  ['Player 1', 'Player 2'] )
+            self.setBattleField( [self.archivePlayer1(), self.archivePlayer2()], 0 ,  ['Player 1', 'Player 2'] )
             print('uploading player data')    
-
-    def mousePress( self, xy , button ) :
-        status = super().mousePress( xy, button )
-        if status == GS.SET_PLAYER_CONFIRM :
-            self.playerSetupSeq()
 
     def draw( self ) :
         self.batch.draw()
