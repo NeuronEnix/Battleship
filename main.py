a6a92e9eb738efbb6912db1d019a1e8b59ca0cc0
neuron909 <kaushikrb909@gmail.com>
2020-05-20 15:34:06 +0530

    made screen point to primary screen

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import Battleship as BS
 
 screens = py.canvas.get_display().get_screens()
 if __name__ == "__main__":
-	window = BS.Battleship	  ( fullscreen = True, screen = screens[1] )
+	window = BS.Battleship	  ( fullscreen = True, screen = screens[0] )
 	py.clock.schedule_interval( window.update	 , window.frame_rate   )
 	window.on_draw()
 	py.app.run    ()
