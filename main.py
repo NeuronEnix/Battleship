684a7c32397d6058f2440bba30b6fb3dcfda89d6
neuron909 <kaushikrb909@gmail.com>
2020-05-20 11:32:43 +0530

    added screens

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pyglet as py
 import Battleship as BS
+
+screens = py.canvas.get_display().get_screens()
 if __name__ == "__main__":
-	window = BS.Battleship(1366,768,fullscreen = False)
-	# window = BS.Battleship(fullscreen = True)
-	py.clock.schedule_interval(window.update, window.frame_rate)
+	window = BS.Battleship	  ( fullscreen = True, screen = screens[1] )
+	py.clock.schedule_interval( window.update	 , window.frame_rate   )
 	window.on_draw()
-	py.app.run()
-   
-   
+	py.app.run    ()
